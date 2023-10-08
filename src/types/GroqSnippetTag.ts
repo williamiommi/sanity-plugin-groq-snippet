279a6cc319ad790d651ad379598c284a15eabf6b
William Iommi <william.iommi@gmail.com>
2023-10-08 15:28:26 +0200

    chore: minor on tag

--- src/types/GroqSnippetTag.ts
+++ src/types/GroqSnippetTag.ts
@@ -2,11 +2,11 @@ import {SanityDocumentLike, Slug} from 'sanity'
 
 export const GROQ_SNIPPET_TAG_TYPE = 'groq.snippet.tag'
 
-export interface GroqSnippetTagMutation {
+interface GroqSnippetTag extends SanityDocumentLike {
   _type: typeof GROQ_SNIPPET_TAG_TYPE
   name: Slug
 }
 
-interface GroqSnippetTag extends GroqSnippetTagMutation, Omit<SanityDocumentLike, '_type'> {}
+export type GroqSnippetTagMutation = Pick<GroqSnippetTag, '_type' | 'name'>
 
 export default GroqSnippetTag
