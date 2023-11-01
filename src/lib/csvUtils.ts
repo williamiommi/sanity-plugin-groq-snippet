4492e7b4d6e939449fc66293bf4fd1d74ab8d17e
William Iommi <william.iommi@gmail.com>
2023-11-01 14:23:02 +0100

    chore: csv commented

--- src/lib/csvUtils.ts
+++ src/lib/csvUtils.ts
@@ -1,32 +1,32 @@
-import Papa from 'papaparse'
-import {GroqSnippetExport} from '../types/GroqSnippet'
+// import Papa from 'papaparse'
+// import {GroqSnippetExport} from '../types/GroqSnippet'
 
-export const CsvHeader = ['Id', 'Title', 'Description', 'Tags', 'Query', 'QueryParams']
-const CsvHeaderAlternative = CsvHeader.filter((header) => header !== 'Id')
+// export const CsvHeader = ['Id', 'Title', 'Description', 'Tags', 'Query', 'QueryParams']
+// const CsvHeaderAlternative = CsvHeader.filter((header) => header !== 'Id')
 
-export const validateCsvHeader = (data: string[]): boolean => {
-  return data.every((item) => CsvHeader.includes(item) || CsvHeaderAlternative.includes(item))
-}
+// export const validateCsvHeader = (data: string[]): boolean => {
+//   return data.every((item) => CsvHeader.includes(item) || CsvHeaderAlternative.includes(item))
+// }
 
-export const parseCsvFile = <T>(
-  file: File,
-  completeCb: (result: Papa.ParseResult<T>) => void,
-): void => {
-  Papa.parse<T>(file, {
-    complete: completeCb,
-  })
-}
+// export const parseCsvFile = <T>(
+//   file: File,
+//   completeCb: (result: Papa.ParseResult<T>) => void,
+// ): void => {
+//   Papa.parse<T>(file, {
+//     complete: completeCb,
+//   })
+// }
 
-export const unparseCsvFile = (rows: GroqSnippetExport[]): string => {
-  return Papa.unparse({
-    fields: CsvHeader,
-    data: rows.map((row) => [
-      row._id,
-      row.title,
-      row.description,
-      row.tags,
-      row.query,
-      row.queryParams,
-    ]),
-  })
-}
+// export const unparseCsvFile = (rows: GroqSnippetExport[]): string => {
+//   return Papa.unparse({
+//     fields: CsvHeader,
+//     data: rows.map((row) => [
+//       row._id,
+//       row.title,
+//       row.description,
+//       row.tags,
+//       row.query,
+//       row.queryParams,
+//     ]),
+//   })
+// }
